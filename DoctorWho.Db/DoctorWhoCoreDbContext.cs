71e3a9c8da540157e8839cc2cbf3210d3c6eb7be
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2023-01-30 02:20:18 +0200

    seeding data

--- DoctorWho.Db/DoctorWhoCoreDbContext.cs
+++ DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -32,6 +32,146 @@ namespace DoctorWho.Db
             modelBuilder.Entity<Episode>().Property(e => e.Title).HasColumnType("varchar(100)");
             modelBuilder.Entity<Episode>().Property(e => e.Notes).HasColumnType("varchar(200)");
 
+
+
+            modelBuilder.Entity<Enemy>().HasData(
+                new Enemy { Id = 1, Name = "Enemy1", Description = "Description1" },
+                new Enemy { Id = 2, Name = "Enemy2", Description = "Description2" },
+                new Enemy { Id = 3, Name = "Enemy3", Description = "Description3" },
+                new Enemy { Id = 4, Name = "Enemy4", Description = "Description4" },
+                new Enemy { Id = 5, Name = "Enemy5", Description = "Description5" }
+                );
+
+            modelBuilder.Entity<Companion>().HasData(
+                new Companion { Id = 1, Name = "Companion1", WhoPlayed = "x" },
+                new Companion { Id = 2, Name = "Companion2", WhoPlayed = "y" },
+                new Companion { Id = 3, Name = "Companion3", WhoPlayed = "z" },
+                new Companion { Id = 4, Name = "Companion4", WhoPlayed = "e" },
+                new Companion { Id = 5, Name = "Companion5", WhoPlayed = "w" }
+                );
+
+            modelBuilder.Entity<Doctor>().HasData(
+                new Doctor { Id = 1, Number = 1, Name = "Doctor1", BirthDate = new DateTime(1900, 02, 04), FirstEpisodeDate = new DateTime(1912, 01, 03), LastEpisodeDate = new DateTime(1941, 03, 02) },
+                new Doctor { Id = 2, Number = 2, Name = "Doctor2", BirthDate = new DateTime(1950, 02, 07), FirstEpisodeDate = new DateTime(1950, 12, 09), LastEpisodeDate = new DateTime(1901, 01, 11) },
+                new Doctor { Id = 3, Number = 3, Name = "Doctor3", BirthDate = new DateTime(1959, 07, 07), FirstEpisodeDate = new DateTime(1900, 05, 03), LastEpisodeDate = new DateTime(1944, 08, 18) },
+                new Doctor { Id = 4, Number = 4, Name = "Doctor4", BirthDate = new DateTime(1927, 07, 12), FirstEpisodeDate = new DateTime(1924, 12, 08), LastEpisodeDate = new DateTime(1941, 03, 21) },
+                new Doctor { Id = 5, Number = 5, Name = "Doctor5", BirthDate = new DateTime(1992, 07, 12), FirstEpisodeDate = new DateTime(1942, 03, 07), LastEpisodeDate = new DateTime(1944, 12, 06) }
+                );
+
+            modelBuilder.Entity<Author>().HasData(
+                new Author { Id = 1, Name = "Author1" },
+                new Author { Id = 2, Name = "Author2" },
+                new Author { Id = 3, Name = "Author3" },
+                new Author { Id = 4, Name = "Author4" },
+                new Author { Id = 5, Name = "Author5" }
+                );
+
+            modelBuilder.Entity<Episode>().HasData(
+                new Episode
+                {
+                    Id = 1,
+                    SeriesNumber = 1,
+                    Number = 1,
+                    Type = "type1",
+                    Title = "Title1",
+                    Date = new DateTime(1900, 02, 04),
+                    Notes = "Note1",
+                    AuthorId = 3,
+                    DoctorId = 1
+                },
+                new Episode
+                {
+                    Id = 2,
+                    SeriesNumber = 2,
+                    Number = 2,
+                    Type = "type2",
+                    Title = "Title2",
+                    Date = new DateTime(1900, 02, 04),
+                    Notes = "Note2",
+                    AuthorId = 2,
+                    DoctorId = 5
+                },
+                new Episode
+                {
+                    Id = 3,
+                    SeriesNumber = 3,
+                    Number = 3,
+                    Type = "type3",
+                    Title = "Title3",
+                    Date = new DateTime(1900, 02, 04),
+                    Notes = "Note3",
+                    AuthorId = 2,
+                    DoctorId = 2
+                },
+                new Episode
+                {
+                    Id = 4,
+                    SeriesNumber = 4,
+                    Number = 4,
+                    Type = "type4",
+                    Title = "Title4",
+                    Date = new DateTime(1900, 02, 04),
+                    Notes = "Note4",
+                    AuthorId = 1,
+                    DoctorId = 5
+                },
+                new Episode
+                {
+                    Id = 5,
+                    SeriesNumber = 5,
+                    Number = 5,
+                    Type = "type5",
+                    Title = "Title5",
+                    Date = new DateTime(1900, 02, 04),
+                    Notes = "Note5",
+                    AuthorId = 3,
+                    DoctorId = null
+                },
+                new Episode
+                {
+                    Id = 6,
+                    SeriesNumber = 6,
+                    Number = 6,
+                    Type = "type6",
+                    Title = "Title6",
+                    Date = new DateTime(1900, 02, 04),
+                    Notes = "Note6",
+                    AuthorId = 2,
+                    DoctorId = 1
+                },
+                new Episode
+                {
+                    Id = 7,
+                    SeriesNumber = 7,
+                    Number = 7,
+                    Type = "type7",
+                    Title = "Title7",
+                    Date = new DateTime(1900, 02, 04),
+                    Notes = "Note7",
+                    AuthorId = 5,
+                    DoctorId = null
+                }
+                );
+
+            modelBuilder.Entity<CompanionEpisode>().HasData(
+                new CompanionEpisode { Id = 1, CompanionId = 2, EpisodeId = 3, },
+                new CompanionEpisode { Id = 2, CompanionId = 1, EpisodeId = 1, },
+                new CompanionEpisode { Id = 3, CompanionId = 2, EpisodeId = 2, },
+                new CompanionEpisode { Id = 4, CompanionId = 3, EpisodeId = 2, },
+                new CompanionEpisode { Id = 5, CompanionId = 5, EpisodeId = 4 }
+                );
+
+            modelBuilder.Entity<EnemyEpisode>().HasData(
+                new EnemyEpisode { Id = 1, EnemyId = 2, EpisodeId = 3, },
+                new EnemyEpisode { Id = 2, EnemyId = 1, EpisodeId = 2, },
+                new EnemyEpisode { Id = 3, EnemyId = 5, EpisodeId = 2, },
+                new EnemyEpisode { Id = 4, EnemyId = 2, EpisodeId = 2, },
+                new EnemyEpisode { Id = 5, EnemyId = 5, EpisodeId = 1, },
+                new EnemyEpisode { Id = 6, EnemyId = 3, EpisodeId = 3, },
+                new EnemyEpisode { Id = 7, EnemyId = 2, EpisodeId = 4, },
+                new EnemyEpisode { Id = 8, EnemyId = 2, EpisodeId = 1, },
+                new EnemyEpisode { Id = 9, EnemyId = 1, EpisodeId = 3, }
+                );
         }
 
         public DbSet<Author> authors { get; set; }
---
+++ DoctorWho.Db/Migrations/20230130001935_seedingData.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DoctorWho.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedingData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "authors",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Author1" },
+                    { 2, "Author2" },
+                    { 3, "Author3" },
+                    { 4, "Author4" },
+                    { 5, "Author5" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "companions",
+                columns: new[] { "Id", "Name", "WhoPlayed" },
+                values: new object[,]
+                {
+                    { 1, "Companion1", "x" },
+                    { 2, "Companion2", "y" },
+                    { 3, "Companion3", "z" },
+                    { 4, "Companion4", "e" },
+                    { 5, "Companion5", "w" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "doctors",
+                columns: new[] { "Id", "BirthDate", "FirstEpisodeDate", "LastEpisodeDate", "Name", "Number" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1900, 2, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1912, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1941, 3, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Doctor1", 1 },
+                    { 2, new DateTime(1950, 2, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1950, 12, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1901, 1, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), "Doctor2", 2 },
+                    { 3, new DateTime(1959, 7, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1900, 5, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1944, 8, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), "Doctor3", 3 },
+                    { 4, new DateTime(1927, 7, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1924, 12, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1941, 3, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Doctor4", 4 },
+                    { 5, new DateTime(1992, 7, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1942, 3, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1944, 12, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "Doctor5", 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "enemys",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Description1", "Enemy1" },
+                    { 2, "Description2", "Enemy2" },
+                    { 3, "Description3", "Enemy3" },
+                    { 4, "Description4", "Enemy4" },
+                    { 5, "Description5", "Enemy5" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "episodes",
+                columns: new[] { "Id", "AuthorId", "Date", "DoctorId", "Notes", "Number", "SeriesNumber", "Title", "Type" },
+                values: new object[,]
+                {
+                    { 1, 3, new DateTime(1900, 2, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "Note1", 1, 1, "Title1", "type1" },
+                    { 2, 2, new DateTime(1900, 2, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), 5, "Note2", 2, 2, "Title2", "type2" },
+                    { 3, 2, new DateTime(1900, 2, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "Note3", 3, 3, "Title3", "type3" },
+                    { 4, 1, new DateTime(1900, 2, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), 5, "Note4", 4, 4, "Title4", "type4" },
+                    { 5, 3, new DateTime(1900, 2, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Note5", 5, 5, "Title5", "type5" },
+                    { 6, 2, new DateTime(1900, 2, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "Note6", 6, 6, "Title6", "type6" },
+                    { 7, 5, new DateTime(1900, 2, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Note7", 7, 7, "Title7", "type7" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "episodeCompanions",
+                columns: new[] { "Id", "CompanionId", "EpisodeId" },
+                values: new object[,]
+                {
+                    { 1, 2, 3 },
+                    { 2, 1, 1 },
+                    { 3, 2, 2 },
+                    { 4, 3, 2 },
+                    { 5, 5, 4 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "episodeEnemies",
+                columns: new[] { "Id", "EnemyId", "EpisodeId" },
+                values: new object[,]
+                {
+                    { 1, 2, 3 },
+                    { 2, 1, 2 },
+                    { 3, 5, 2 },
+                    { 4, 2, 2 },
+                    { 5, 5, 1 },
+                    { 6, 3, 3 },
+                    { 7, 2, 4 },
+                    { 8, 2, 1 },
+                    { 9, 1, 3 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "authors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "companions",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "doctors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "doctors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "enemys",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "episodeCompanions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "episodeCompanions",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "episodeCompanions",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "episodeCompanions",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "episodeCompanions",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "episodeEnemies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "episodeEnemies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "episodeEnemies",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "episodeEnemies",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "episodeEnemies",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "episodeEnemies",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "episodeEnemies",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "episodeEnemies",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "episodeEnemies",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "episodes",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "episodes",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "episodes",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "authors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "companions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "companions",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "companions",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "companions",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "enemys",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "enemys",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "enemys",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "enemys",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "episodes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "episodes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "episodes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "episodes",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "authors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "authors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "authors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "doctors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "doctors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "doctors",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
