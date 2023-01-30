f00cb5ffa4d5fbbfe03c8f9a288e7dc14735602f
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2023-01-30 02:23:26 +0200

    add View and Sp

---
+++ DoctorWho.Db/Migrations/20230130002151_addView.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorWho.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class addView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE VIEW viewEpisodes AS
+                                    SELECT E.Id, Title, A.Name AS AutherName, D.Name AS DoctorName, dbo.fnCompanions(E.Id) AS Companions, dbo.fnEnemies(E.Id) AS Enemies
+                                    FROM episodes E
+                                    JOIN doctors D ON E.DoctorId = D.Id
+                                    JOIN authors A ON E.AuthorId = A.Id;"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP VIEW IF EXISTS viewEpisodes;");
+        }
+    }
+}
---
+++ DoctorWho.Db/Migrations/20230130002245_addSP.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorWho.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROCEDURE spSummariseEpisodes
+                                    AS
+                                    BEGIN	
+	                                    SELECT TOP 3 C.Name, COUNT(*) AS CountAppearing
+	                                    FROM episodeCompanions EC LEFT JOIN companions C
+	                                    ON C.Id = EC.CompanionId 
+	                                    GROUP BY C.Name
+	                                    ORDER BY COUNT(*) DESC;
+
+	                                    SELECT TOP 3 E.Name, COUNT(*) AS CountAppearing
+	                                    FROM episodeEnemies EE LEFT JOIN enemys E  ON E.Id = EE.EnemyId 
+	                                    GROUP BY E.Name
+	                                    ORDER BY COUNT(*) DESC;
+                                    END;"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE IF EXISTS spSummariseEpisodes;");
+        }
+    }
+}
