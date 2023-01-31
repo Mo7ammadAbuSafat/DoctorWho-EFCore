390d8216842802b3a15261f77d902d3586912889
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2023-01-31 13:37:01 +0200

    add CRUD operation class

---
+++ DoctorWho.Db/Services/CRUDoperaations.cs
@@ -0,0 +1,38 @@
+﻿namespace DoctorWho.Db.Services
+{
+    public class CRUDoperaations<T> where T : class
+    {
+        private DoctorWhoCoreDbContext context { get; set; }
+        public CRUDoperaations(DoctorWhoCoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(T Item)
+        {
+            context.Add(Item);
+            context.SaveChanges();
+        }
+
+        public void Update(T Item)
+        {
+            context.Update(Item);
+            context.SaveChanges();
+        }
+
+        public void Delete(T Item)
+        {
+            context.Remove(Item);
+            context.SaveChanges();
+        }
+
+        public void Delete(int Id)
+        {
+            var enemy = context.Set<T>().Find(Id);
+            if (enemy != null)
+            {
+                Delete(enemy);
+            }
+        }
+    }
+}
