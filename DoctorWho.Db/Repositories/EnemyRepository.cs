343e493347117a0dbbe530d44a938c667167f558
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2023-01-31 14:21:52 +0200

    refactor code and add repositories

---
+++ DoctorWho.Db/Repositories/EnemyRepository.cs
@@ -0,0 +1,30 @@
+﻿using DoctorWho.Db.Models;
+using DoctorWho.Db.Services;
+
+namespace DoctorWho.Db.Repositories
+{
+    public class EnemyRepository
+    {
+        private DoctorWhoCoreDbContext context;
+        public CRUDoperaations<Enemy> CRUD { get; set; }
+
+        public EnemyRepository(DoctorWhoCoreDbContext context)
+        {
+            this.context = context;
+            CRUD = new CRUDoperaations<Enemy>(context);
+        }
+
+        public List<Enemy> GetAllEnemies()
+        {
+            return context.enemys.ToList();
+        }
+
+        public Enemy? GetEnemyById(int id)
+        {
+            return context.enemys.Find(id);
+        }
+
+
+
+    }
+}
