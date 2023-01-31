125f130b6d6383e6830017b5af4a6c594973b2a8
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2023-01-31 14:04:42 +0200

    add General services

--- DoctorWho/Program.cs
+++ DoctorWho/Program.cs
@@ -1,6 +1,10 @@
 ﻿using DoctorWho.Db;
+using DoctorWho.Db.Services;
 
 using (var context = new DoctorWhoCoreDbContext())
 {
+    var service = new GeneralServices(context);
+
+    service.AddEnemyToEpisode(4, 1);
 
 }
